d42154a6c3626299467bdeeec0c55fa23ee13114
Matt Kynaston <matt@claritum.com>
2012-06-29 17:24:41 +0100

    ZF-12225 - Fixed classname in Zend_Cloud_StorageServer_Rackspace

--- library/Zend/Cloud/StorageService/Adapter/Rackspace.php
+++ library/Zend/Cloud/StorageService/Adapter/Rackspace.php
@@ -68,7 +68,7 @@ class Zend_Cloud_StorageService_Adapter_Rackspace
         }
 
         try {
-            $this->_rackspace = new Zend_Service_Rackspace_File($options[self::USER], $options[self::API_KEY]);
+            $this->_rackspace = new Zend_Service_Rackspace_Files($options[self::USER], $options[self::API_KEY]);
         } catch (Zend_Service_Rackspace_Exception $e) {
             throw new Zend_Cloud_StorageService_Exception('Error on create: '.$e->getMessage(), $e->getCode(), $e);
         }
